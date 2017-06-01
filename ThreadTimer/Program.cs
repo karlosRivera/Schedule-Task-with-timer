3fed61f113e367daf06b15fb852122e576d9c562
tridipkolkata <tridip2000@gmail.com>
2017-06-01 16:29:52 +0530

    1st commit

---
+++ ThreadTimer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ThreadTimer
+{
+    class Program
+    {
+        int i = 1;
+        static Timer timer;
+
+        static void Main(string[] args)
+        {
+            schedule_Timer();
+            Console.ReadLine();
+        }
+
+        static void schedule_Timer()
+        {
+            Console.WriteLine("### Timer Started ###");
+
+            DateTime nowTime = DateTime.Now;
+            DateTime scheduledTime = new DateTime(nowTime.Year, nowTime.Month, nowTime.Day, 8, 42, 0, 0); 
+            //Specify your scheduled time HH,MM,SS [8am and 42 minutes]
+
+            if (nowTime > scheduledTime)
+            {
+                scheduledTime = scheduledTime.AddDays(1);
+            }
+
+            double tickTime = (double)(scheduledTime - DateTime.Now).TotalMilliseconds;
+            timer = new Timer(tickTime);
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            timer.Start();
+        }
+
+        static void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("### Timer Stopped ### \n");
+            timer.Stop();
+            Console.WriteLine("### Scheduled Task Started ### \n\n");
+            Console.WriteLine("Hello World!!! - Performing scheduled task\n");
+            Console.WriteLine("### Task Finished ### \n\n");
+            schedule_Timer();
+        }
+    }
+}
